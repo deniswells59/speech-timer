f32be4d61fa72fa4d6c52d3d350a48e921fc6644
Denis Wells <deniswells59@gmail.com>
2024-10-01 18:15:58 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Speech and Debate Timer</h1>
+    </div>
+  </header>
+
+  <main>
+    <h3>Timer</h3>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
