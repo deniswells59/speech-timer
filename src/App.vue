c0c22e35bf909b042aa251ef330e57d91d822f42
Denis Wells <deniswells59@gmail.com>
2024-10-02 10:12:26 -0700

    fix timer bugs

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,14 @@ import { store } from './store';
 
   <main>
     <h3>
-      <span>{{ Boolean(store.minutes) && store.minutes < 10 }}</span> {{ store.minutes ?? '00' }} : {{ store.seconds ?? '00' }}</h3>
+      <span v-if="(Boolean(store.minutes) || store.minutes === 0) && store.minutes < 10">0</span>
+      <span>{{ store.minutes ?? '00' }}</span>
+      <span>:</span>
+      <span v-if="(Boolean(store.seconds) || store.seconds === 0) && store.seconds < 10">0</span>
+      <span>{{ store.seconds ?? '00' }}</span>
+    </h3>
 
-    <button @click="store.setTimer(7)">Start Timer</button>
+    <button @click="store.startTimer(1)">Start Timer</button>
     <button @click="store.resetTimer()">Reset Timer</button>
   </main>
 </template>
--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
 import moment from 'moment';
 import { reactive } from 'vue';
 
+// The delay after the user clicks to start the timer
+// (This makes for a better UX where the user can easily see the initial
+//  start time before the countdown begins)
+const INPUT_DELAY_IN_MS = 1200;
+
+// How often we check the time
+const TIMER_INTERVAL_IN_MS = 100;
+
 export const store = reactive({
   data() {
     return {
       minutes: null,
       seconds: null,
       interval: null,
+      countDownDate: null,
     };
   },
   resetTimer() {
@@ -14,26 +23,41 @@ export const store = reactive({
     this.seconds = null;
     clearInterval(this.interval);
   },
-  setTimer(timeinMinutes) {
+  startTimer(timeinMinutes) {
     this.resetTimer();
-    const countDownDate = moment().add(timeinMinutes, 'm').toDate();
-    console.log('start');
-    this.interval = setInterval(() => {
-      const now = moment();
-      const diff = countDownDate - now;
+    this.initTimer(timeinMinutes);
 
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    setTimeout(() => {
+      // Calculates when timer should end
+      this.countDownDate = moment().add(timeinMinutes, 'm').toDate();
 
-      this.minutes = minutes;
-      this.seconds = seconds;
-    }, 10);
+      this.interval = setInterval(() => {
+        this.setTimerOnInterval();
+      }, TIMER_INTERVAL_IN_MS);
+
+      // Call the timer while we wait for our interval
+      // fixes edge case where a second appears to be skipped due while
+      // waiting for initial interval time
+      this.setTimerOnInterval();
+    }, INPUT_DELAY_IN_MS);
   },
-  setInterval() {
-    this.interval = setInterval(() => {
-      this.timer = this.timer - 1;
-    }, 1000);
+  // Shows initial time
+  initTimer(timeInMinutes) {
+    this.minutes = timeInMinutes;
+  },
+  //
+  setTimerOnInterval() {
+    const now = moment();
+    const diff = this.countDownDate - now;
 
-    console.log(this.interval);
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    this.minutes = minutes;
+    this.seconds = seconds;
+
+    if (this.minutes <= 0 && this.seconds <= 0) {
+      this.resetTimer();
+    }
   },
 });
